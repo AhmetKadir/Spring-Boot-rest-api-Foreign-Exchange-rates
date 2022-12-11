330ab4f3efdacb92dcd5fa2bc98f8cc1f2ae1270
AhmetKadir <ahmetkadir518@gmail.com>
2022-12-11 19:01:55 +0300

    Update ForeignCurrencyController.java

--- foreign-exchange-service/src/main/java/com/aka/foreignexchangeservice/controller/ForeignCurrencyController.java
+++ foreign-exchange-service/src/main/java/com/aka/foreignexchangeservice/controller/ForeignCurrencyController.java
@@ -14,27 +14,8 @@ import com.aka.foreignexchangeservice.repository.ForeignCurrencyDao;
 @RestController
 @RequestMapping("/api")
 public class ForeignCurrencyController {
-	
-	@Autowired
-    private ApiClient apiClient;
-
-	/*@Autowired
-	private ForeignCurrencyRepository repository;*/
 
 	@Autowired
 	private ForeignCurrencyDao dao;
 
-	//private static final Logger logger = LoggerFactory.getLogger(ForeignCurrencyController.class);
-
-	@GetMapping("/euro")
-    public ForeignCurrency getForeignCurrency() {
-        ForeignCurrency foreignCurrency = apiClient.getForeignCurrency();
-		System.out.println(foreignCurrency.toString());
-		return dao.save(foreignCurrency);
-    }
-
-	/*public Mono<ServerResponse> getForeignCurrency(ServerRequest request) { 
-		Mono<ForeignCurrency> currency = request.bodyToMono(ForeignCurrency.class);
-		return ServerResponse.ok().build(repository.save(currency));
-	}*/
 }
