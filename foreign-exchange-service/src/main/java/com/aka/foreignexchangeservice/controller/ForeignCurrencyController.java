c08350d0d32eab89a44b400995eb62a92edb9495
AhmetKadir <ahmetkadir518@gmail.com>
2022-12-30 20:09:37 +0300

    Update ForeignCurrencyController.java

--- foreign-exchange-service/src/main/java/com/aka/foreignexchangeservice/controller/ForeignCurrencyController.java
+++ foreign-exchange-service/src/main/java/com/aka/foreignexchangeservice/controller/ForeignCurrencyController.java
@@ -1,11 +1,14 @@
 package com.aka.foreignexchangeservice.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.aka.foreignexchangeservice.model.ForeignCurrency;
 import com.aka.foreignexchangeservice.repository.ForeignCurrencyRepository;
@@ -28,7 +31,49 @@ public class ForeignCurrencyController {
 		return repository.findByApiSource(apiSource);
 	}
 
-	@GetMapping("base/{baseCurrency}")
+	@GetMapping("showCurrencies")
+	public List<ForeignCurrency> showCurrencies(@RequestParam(required = false) String baseCurrency,
+			@RequestParam(required = false) String apiSource, @RequestParam(required = false) String day,
+			@RequestParam(required = false) String time) {
+		
+		// Initialize an empty list of ForeignCurrency objects to store the results
+		List<ForeignCurrency> currencies = new ArrayList<>();
+		
+		// Retrieve the currencies with the specified base currency
+		if (baseCurrency != null) {
+			currencies = repository.findByBase(baseCurrency);
+		}
+		else{
+			currencies = repository.findAll();
+		}
+		
+		// Filter the currencies by the specified api source
+		if (apiSource != null) {
+			currencies = currencies.stream()
+					.filter(currency -> currency.getApiSource().equals(apiSource))
+					.collect(Collectors.toList());
+		}
+		
+		// Filter the currencies by the specified day
+		if (day != null) {
+			currencies = currencies.stream()
+					.filter(currency -> currency.getDay().equals(day))
+					.collect(Collectors.toList());
+		}
+		
+		// Filter the currencies by the specified time
+		if (time != null) {
+			currencies = currencies.stream()
+					.filter(currency -> currency.getTime().equals(time))
+					.collect(Collectors.toList());
+		}
+		
+		// Return the filtered list of currencies
+		return currencies;
+	}
+	
+
+	@GetMapping("currency/{baseCurrency}")
 	public List<ForeignCurrency> getByBaseCurrency(@PathVariable String baseCurrency) {
 		return repository.findByBase(baseCurrency);
 	}
@@ -41,6 +86,11 @@ public class ForeignCurrencyController {
 	@GetMapping("date/{day}/{time}")
 	public List<ForeignCurrency> getByDayAndTime(@PathVariable String day, @PathVariable String time) {
 		return repository.findByDayAndTime(day, time);
+	}
+
+	@GetMapping("date/{day}/{time1}/{time2}")
+	public List<ForeignCurrency> getByDayAndTimeRange(@PathVariable String day, @PathVariable String time1, @PathVariable String time2) {
+		return repository.findByDayAndTimeBetween(day, time1, time2);
 	}
 
 }
